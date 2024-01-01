832fa1ef69557b49761efbd820926dba9a4a28e1
carlos <ccarbonell@gmail.com>
2024-01-01 15:30:34 +0100

    structure changes

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo apt install portaudio19-dev
+sudo apt-get install libsndfile1-dev
+sudo apt-get install libportmidi-dev libportmidi0
+sudo apt-get install liblo-dev
+pip install -r requirements.txt
