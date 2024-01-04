27556749eef6e5d7ea6466cdca6df4c1dd91cb06
carlos <ccarbonell@gmail.com>
2024-01-04 15:25:06 +0100

    Update test_stereo.py

--- tests/test_stereo.py
+++ tests/test_stereo.py
@@ -2,11 +2,11 @@ from pyo import *
 from time import sleep
 
 # Initialize Pyo server
-s = Server(sr=44100, buffersize=4056,audio="jack")
+s = Server(audio="jack")
 sleep(5)
 pa_list_devices()
 # Set the input/output device to device number 12
-s.setInOutDevice(0)
+s.setInOutDevice(2)
 
 # Boot the server
 s.boot()
