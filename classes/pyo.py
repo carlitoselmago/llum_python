5fd76a86baad7cb2f0ed08abdd84c951244202c6
carlitoselmago <ccarbonell@gmail.com>
2024-01-29 23:11:45 +0100

    added test mixer

--- classes/pyo.py
+++ classes/pyo.py
@@ -45,77 +45,12 @@ class Sound:
         #synth part
         s.start()
         #pa_list_devices()
-        """
-        #pitches = [midiToHz(m) for m in [36, 43, 48, 55, 60, 62, 64, 65, 67, 69, 71, 72]]
-
-        # Create a white noise generator
-        noise = Noise()
-
-        # Use a low-pass filter with noise as input
-        filt = ButLP(noise)  # Stereo output
-
-        # LFO to modulate filter frequency
-        lfo = Sine(freq=0.1, mul=100, add=500)
-        filt.freq = lfo
-
-        # Reverb for spatial effect
-        rev = Freeverb(filt)  # Stereo output
-
-        # Create a stereo mixer with 2 channels (left and right)
-        mixer = Mixer(outs=2)
-
-        # Add the noise to both the left (0) and right (1) channels of the mixer
-        mixer.addInput(0, filt)
-        mixer.setAmp(0, 0, 1)  # Channel 0 input to left output
-        mixer.setAmp(0, 1, 1)  # Channel 0 input to right output
-
-        # Output the mixed audio
-        mixer.out()
-        """
-        #####################################################################
+        
 
         # Load audio files
         audio1 = SfPlayer("audio1.wav", loop=True)
         audio2 = SfPlayer("audio2.wav", loop=True).out()
-        """
-        # Reverb 1 ##############################################################
-        #rev = Freeverb(audio1, size=0.8, damp=0.7, bal=0.5).out()
-
-        # reverb alternative ####################################################
-        # Four parallel stereo comb filters. The delay times are chosen
-        # to be as uncorrelated as possible. Prime numbers are a good
-        # choice for delay lengths in samples.
-        comb1 = Delay(audio1, delay=[0.0297, 0.0277], feedback=0.65)
-        comb2 = Delay(audio1, delay=[0.0371, 0.0393], feedback=0.51)
-        comb3 = Delay(audio1, delay=[0.0411, 0.0409], feedback=0.5)
-        comb4 = Delay(audio1, delay=[0.0137, 0.0155], feedback=0.73)
-
-        combsum = audio1 + comb1 + comb2 + comb3 + comb4
-
-        # The sum of the original signal and the comb filters
-        # feeds two serial allpass filters.
-        all1 = Allpass(combsum, delay=[0.005, 0.00507], feedback=0.75)
-        all2 = Allpass(all1, delay=[0.0117, 0.0123], feedback=0.61)
-
-        # Brightness control.
-        lowp = Tone(all2, freq=3500, mul=0.25).out()
-
-        # END reverb 2 ###########################################
-
-        # Apply filter to audio1
-        #filt = ButBP(audio1, freq=100, q=5).out()
-        freq = Sig(1000)
-        freq.ctrl([SLMap(50, 5000, "lin", "value", 1000)], title="Cutoff Frequency")
-
-        # Three different lowpass filters
-        tone = Tone(, freq)
-        butlp = ButLP(n, freq)
-        mooglp = MoogLP(n, freq)
-
-        # Interpolates between input objects to produce a single output
-        sel = Selector([tone, butlp, mooglp]).out()
-        """
-
+        
         # Common cutoff frequency control
         freq = Sig(1000)
         #freq.ctrl([SLMap(50, 5000, "lin", "value", 1000)], title="Cutoff Frequency")
@@ -139,16 +74,7 @@ class Sound:
            
         #############################################################################
 
-        """
-        #set controllers (4 of them)
-        controllers={
-            0:filt.freq,
-            1:lfo.mul,
-            2:lfo.add,
-           # 3:mixer.amp
-        }
-        """
-
+      
         while not self.terminate_flag.value:
             
             if not self.queue.empty():
@@ -187,15 +113,10 @@ class Sound:
                         val=self.getData(index) 
                         #print(val)
                         rev.bal=val
-                    """
-                    if index==3:
-                        pass
-                    """
-                #dmx_data = self.queue.get()
-                #print(dmx_data)
-                #filt.freq=self.dmxosc.scale_single_value(dmx_data,-50,50,400,1800)
+                  
+              
                 
-            #sleep(0.0001)
+         
             pass
         print("ENDDDD")
         sys.exit()
