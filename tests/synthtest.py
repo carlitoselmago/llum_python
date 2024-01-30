f167353c9d10cfa32b4463a81c94ce01fd887c03
carlitoselmago <ccarbonell@gmail.com>
2024-01-30 01:08:24 +0100

    Update synthtest.py

--- tests/synthtest.py
+++ tests/synthtest.py
@@ -1,6 +1,6 @@
 from pyo import *
 s=Server(sr=44100, nchnls=2, buffersize=256, duplex=1)
-s.setOutputDevice(12)
+#s.setOutputDevice(12)
 s.boot()                    # boot the server
 sin = Sine(mul=0.5)         # start a single sine stream
 sin.out()                   # send this sine stream to audio ouput
